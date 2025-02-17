783faaef8ad5d4705217a43d552ca15acc9b47f1
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2025-02-17 13:46:59 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', function () {
+    return ('Hello');
 });
+
+Route::get('/world', function () {
+    return ('World!');
+});
+
+Route::get('/', function () {
+    return ('Selamat Datang!');
+});
+
+Route::get('/about', function () {
+    return ('NIM : 2341760088 <br> Nama : Erfin Jauhari Dwi Brian');
+});
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya ' . $name;
+// });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+// Route::get('/user/{name?}', function ($name = null) {
+//     return 'Nama saya ' . $name;
+// });
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+
+// ------------------------------------------------------------------------
