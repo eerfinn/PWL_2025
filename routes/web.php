766334a230899c5cf878517cbd23acf437bd6441
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2025-02-23 21:12:57 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,10 @@ Route::get('/world', function () {
 
 // Route::get('/about', [PageController::class, 'about']);
 
+Route::get('/about', function () {
+    return 'NIM : 2341760088 <br> Nama : Erfin Jauhari Dwi Brian';
+});
+
 // Route::get('/user/{name}', function ($name) {
 //     return 'Nama saya ' . $name;
 // });
@@ -43,8 +47,11 @@ Route::get('/world', function () {
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
-
 // Route::get('/articles/{id}', [PageController::class, 'article'] );
+
+Route::get('/articles/{id}', function($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+} );
 
 // Route::get('/user/{name?}', function ($name = null) {
 //     return 'Nama saya ' . $name;
